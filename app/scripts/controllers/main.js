bb18b4aba2eeff69484f0b40f839aea8deac69c7
FabVillegas <f.villegas93@gmail.com>
2015-12-22 12:58:50 -0600

    avances css

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name usariaUxpmApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the usariaUxpmApp
+ */
+angular.module('usariaUxpmApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
